57247cdbb8e16388ac66bb9eadd9e99d19b64a90
Gricel Dayhanna Sepúveda Rozas <gr3c2l@gmail.com>
2017-02-06 09:48:15 -0300

    Fixpop

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,3 @@
-//SCROLL BLOCK POPUP
-function no_scrollbody()
-{
-    $html = $('html'); 
-    $body = $('body'); 
-    var initWidth = $body.outerWidth();
-    var initHeight = $body.outerHeight();
-
-    var scrollPosition = [
-        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-    ];
-    $html.data('scroll-position', scrollPosition);
-    $html.data('previous-overflow', $html.css('overflow'));
-    $html.css('overflow', 'hidden');
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);   
-
-    var marginR = $body.outerWidth()-initWidth;
-    var marginB = $body.outerHeight()-initHeight; 
-    $body.css({'margin-right': marginR,'margin-bottom': marginB});
-}
-function scrollbody()
-{
-    $html = $('html');
-    $body = $('body');
-    $html.css('overflow', $html.data('previous-overflow'));
-    var scrollPosition = $html.data('scroll-position');
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);    
-
-    $body.css({'margin-right': 0, 'margin-bottom': 0});
-        $("body, html").css({
-                        "overflow-x":'hidden'
-        });
-}
 $(document).ready(function(){ 
   //DROP MENU MOBILE
   $(".cg-mobile_menu").click(function()
@@ -45,7 +11,6 @@ $(document).ready(function(){
                                 "overflow-y":'scroll'
                             });
         $(".cg-mobile_menu").addClass("cg-mobile_menu_active");
-        no_scrollbody();
       }
     else{
           $(".cg-main_header > .cg-main > .cg-wrapper").css({
@@ -55,7 +20,6 @@ $(document).ready(function(){
                                 "overflow-y":'hidden'
                             });
           $(".cg-mobile_menu").removeClass("cg-mobile_menu_active");
-          scrollbody();
         }
   });
   //DROP SUBMENU
@@ -89,7 +53,6 @@ $(document).ready(function(){
   $('.cg-main_header > .cg-main > .cg-wrapper .cg-link').click(function() {
       $('.cg-main_header > .cg-main > .cg-wrapper .cg-link').removeClass('cg-active');
       $(this).addClass('cg-active');
-      scrollbody();
   });
 });
 
@@ -109,9 +72,6 @@ function close_login_pop()
   $(".cg-main_header > .cg-main > .cg-wrapper").css({
                                               "height":''
                                           });
-  $(".cg-main_header").css({
-                          "overflow-y":'hidden'
-                        });
   $(".cg-mobile_menu").removeClass("cg-mobile_menu_active");
 }
 
@@ -130,9 +90,6 @@ function sesion_on()
   $(".cg-main_header > .cg-main > .cg-wrapper").css({
                                               "height":''
                                           });
-  $(".cg-main_header").css({
-                          "overflow-y":'hidden'
-                        });
   $(".cg-mobile_menu").removeClass("cg-mobile_menu_active");
 }
 
